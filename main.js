0cff1709d86dd6908885ba0a318284ff6c71e12c
firefoxmoo <firefoxmoo@gmail.com>
2024-01-13 23:15:32 -0700

    File system setup

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var http = require('http');
+var url = require("url");
+var fs = require("fs");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+http.createServer(function (request, response) { // request is object
+  response.writeHead(200, {'Content-Type': 'text/html'}); // (status 200 ok, response headers object)
+  response.write(request.url);
+  var q = url.parse(req.url, true).query;
+  var txt = q.year+" "+q.month;
+  response.end(txt);
 }).listen(8080);
