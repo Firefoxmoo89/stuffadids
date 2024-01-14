6adedca0643fb23b66f55a1fd0c4ae52655eaab6
firefoxmoo <firefoxmoo@gmail.com>
2024-01-14 00:51:06 -0700

    Fixed require for radicalModule

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var http = require('http'); var url = require("url"); var fs = require("fs"); var rad = require("../radicalModule.js");
+var http = require('http'); var url = require("url"); var fs = require("fs"); var rad = require("./radicalModule.js");
 
 http.createServer(function (request, response) { // request is object
   
