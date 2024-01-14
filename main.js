eaf301b8120dc472129729b803f545d295882bb8
firefoxmoo <firefoxmoo@gmail.com>
2024-01-14 00:13:46 -0700

    Structure of basic frontend and backend initialized

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-var http = require('http');
-var url = require("url");
-var fs = require("fs");
+var http = require('http'); var url = require("url"); var fs = require("fs"); var rad = require("../radicalModule.js");
 
 http.createServer(function (request, response) { // request is object
-  response.writeHead(200, {'Content-Type': 'text/html'}); // (status 200 ok, response headers object)
+  deets = url.parse(request.url, true); // deets.query for post ? information as object
+
+  
+  if (deet.pathname == "/") {
+    fs.readFile("templates/index.html", (error, data) {
+      response.writeHead(200, {           // (status 200 ok, response headers object)
+        "Content-Type": "texthtml"
+      });
+      response.write(data);                   // html content
+      response.end();
+    });
+  }
+
+  response.writeHead(200, {'Content-Type': 'text/html'}); 
   response.write(request.url);
   var q = url.parse(req.url, true).query;
   var txt = q.year+" "+q.month;
