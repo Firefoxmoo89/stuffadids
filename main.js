f96213b5c0dfe574c9b9b57e46b101421462bf91
firefoxmoo <firefoxmoo@gmail.com>
2024-01-17 17:21:43 -0700

    Fixed addition of top and bottom html with skeleton header and footer

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
 var http = require('http'); var url = require("url"); var fs = require("fs"); var rad = require("./radicalModule.js");
 
+
 http.createServer(function (request, response) { // request is object
   
   deets = url.parse(request.url, true); // deets.query for post ? information as object
-  fs.readFile("templates/top.html",(error,data) => { htmlTop = data })
-  fs.readFile("templates/bottom.html",(error,data) => {htmlBottom = data})
-  
-  function serveFile(file,status,headers) {
-    fs.readFile(file,(error,data) => {
-      response.writeHead(status,headers);         // (status 200 ok, response headers object)
-      response.write(htmlTop+data+htmlBottom);    // html content
-      response.end();
-    });
-  }
 
   if (deets.pathname == "/") {
-    serveFile("templates/index.html",200,{"Content-type":"text/html"});
+    rad.serveFile("templates/index.html",200,{"Content-type":"text/html"});
   }
 
   else {
-    serveFile("templates/missing.html",404,{"Content-type":"text/html"});
+    rad.serveFile("templates/missing.html",404,{"Content-type":"text/html"});
   }
 }).listen(8080);
--- radicalModule.js
+++ radicalModule.js
@@ -1,6 +1,14 @@
-exports.myDateTime = function () {
-    return Date();
-  };
+exports.serveFile = (file,status,headers) => {
+  fs.readFile("template/top.html",(errorTop,htmlTop) => {
+    fs.readFile("template/bottom.html",(errorBottom,htmlBottom) => {
+      fs.readFile(file,(error,data) => {
+        response.writeHead(status,headers);         // (status 200 ok, response headers object)
+        response.write(htmlTop+data+htmlBottom);    // html content
+        response.end();
+      });
+    });
+  });
+};
 
 // Use the exports keyword to make properties and methods available outside the module file.
 
