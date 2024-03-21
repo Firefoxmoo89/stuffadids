0152132008c8338189c38aa96be8d84b77053de2
Firefoxmoo89 <firefoxmoo@gmail.com>
2024-03-20 21:39:00 -0600

    Clarification of radicalModule

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 var http = require('http'); var url = require("url"); var fs = require("fs"); var rad = require("./radicalModule.js");
 
-
-http.createServer(function (request, response) { // request is object
+http.createServer(function (request, response) { 
   
-  deets = url.parse(request.url, true); // deets.query for post ? information as object
+  deets = url.parse(request.url, true); 
 
   if (deets.pathname == "/") {
-    rad.serveFile("templates/index.html",200,{"Content-type":"text/html"});
+    rad.servePage("templates/index.html",200,{"Content-type":"text/html"},response);
+  }
+  else {
+    rad.servePage("templates/missing.html",404,{"Content-type":"text/html"},response);
   }
 
-  else {
-    rad.serveFile("templates/missing.html",404,{"Content-type":"text/html"});
-  }
-}).listen(8080);
+}).listen(80);
--- radicalModule.js
+++ radicalModule.js
@@ -1,16 +1,44 @@
-var fs = require("fs");
+var fs = require("fs"); var util = require("util");
+//var mysql = require("mysql"); 
 
-exports.serveFile = (file,status,headers) => {
-  fs.readFile("template/top.html",(errorTop,htmlTop) => {
-    fs.readFile("template/bottom.html",(errorBottom,htmlBottom) => {
-      fs.readFile(file,(error,data) => {
-        response.writeHead(status,headers);         // (status 200 ok, response headers object)
-        response.write(htmlTop+data+htmlBottom);    // html content
-        response.end();
-      });
-    });
+exports.serveFile = (file,status,headers,response) => {
+  fs.readFile(file,(error,fileData) => {
+    response.writeHead(status,headers); 
+    response.end(fileData);
   });
 };
 
-// Use the exports keyword to make properties and methods available outside the module file.
+exports.servePage = (page,status,headers,response) => {
+  headers["Content-type"] = "text/html";
+  fs.readFile("html/top.html",(errorTop,htmlTop) => {
+    fs.readFile("html/bottom.html",(errorBottom,htmlBottom) => {
+      fs.readFile("html/"+page+".html",(error,fileData) => {
+        response.writeHead(status,headers);         
+        response.end(htmlTop+fileData+htmlBottom);
+      });
+    });
+  });
+}
 
+exports.processPOST = (request,daFunction) => {
+  var formData = {}; var stream;
+  request.on("data", (chunk) => { stream += chunk } )
+  request.on("end", () => { 
+    chunks = [];
+    while (stream.includes("Content-Disposition")) {
+      start = stream.indexOf("Content-Disposition"); stream = stream.slice(start+"Content-Disposition: ".length,-1)+stream.slice(-1,-1); 
+      end = stream.indexOf("Content-Disposition"); 
+      dataString = stream.slice(0,end); output = {};
+      stream = stream.slice(end,-1)+stream.slice(-1,-1);
+      type = dataString.slice(0,dataString.indexOf(";"));
+      if (!type.includes("image")) {
+        dataString = dataString.slice(dataString.indexOf("\"")+1,-1)+dataString.slice(-1,-1);
+        daKey = dataString.slice(0,dataString.indexOf("\"")); dataString = dataString.replace(daKey+"\"","");
+        dataString = dataString = dataString.slice(dataString.indexOf("\"")+1,-1)+dataString.slice(-1,-1);
+        daValue = dataString.slice(0,dataString.indexOf("\"")); dataString = dataString.replace(daValue+"\"","");
+        formData[daKey] = daValue;
+      }
+    }
+    daFunction(formData);
+  });
+}
