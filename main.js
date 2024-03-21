b851c5c56b3473341aff82277862d086652a90e8
Firefoxmoo89 <firefoxmoo@gmail.com>
2024-03-20 21:44:27 -0600

    Reformatted fs structure, fixed rad import

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 var http = require('http'); var url = require("url"); var fs = require("fs"); var rad = require("./radicalModule.js");
+console.log("http://localhost:8080");
 
 http.createServer(function (request, response) { 
   
   deets = url.parse(request.url, true); 
 
   if (deets.pathname == "/") {
-    rad.servePage("templates/index.html",200,{"Content-type":"text/html"},response);
+    rad.servePage("index",200,{"Content-type":"text/html"},response);
   }
   else {
-    rad.servePage("templates/missing.html",404,{"Content-type":"text/html"},response);
+    rad.servePage("missing",404,{"Content-type":"text/html"},response);
   }
 
-}).listen(80);
+}).listen(8080);
