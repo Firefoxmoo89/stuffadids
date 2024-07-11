058423e9eae08ca34434f3bd909cc3d12a32aff1
Firefoxmoo89 <firefoxmoo@gmail.com>
2024-07-10 18:48:56 -0600

    Updated some basic structure

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-var http = require('http'); var url = require("url"); var fs = require("fs"); var rad = require("./radicalModule.js");
-console.log("http://localhost:8080");
+require("./instrument.js");
+var http = require('http'); var url = require("url"); var fs = require("fs"); 
+var rad = require("./radicalModule.js"); var mail = require("./mail.js");
+
+datetime = new Date(); datetime = "D"+datetime.toISOString().slice(0,19).replaceAll(":","").replaceAll("-","");
+for (html of ["top.html","bottom.html"]) {
+  content = fs.readFileSync("html/"+html,"utf8");
+  content = content.replace(/\?v=.*\"/g,"?v="+datetime+"\"");
+  fs.writeFileSync("html/"+html,content);
+}
 
 http.createServer(function (request, response) { 
-  
   deets = url.parse(request.url, true); 
 
   if (deets.pathname == "/") {
     rad.servePage("index",200,{"Content-type":"text/html"},response);
   }
-  else {
-    rad.servePage("missing",404,{"Content-type":"text/html"},response);
+  else if (deets.pathname.includes("/style")) { rad.serveFile(deets.pathname.slice(1),200,{"Content-type":"text/css"},response) }
+  else if (deets.pathname.includes("/script")) { rad.serveFile(deets.pathname.slice(1),200,{"Content-type":"text/javascript"},response) }
+  else if (deets.pathname.includes("/image")) { 
+    extension = deets.pathname.slice(deets.pathname.indexOf(".")+1);
+    if (extension == "svg") { extension = "svg+xml" }
+    rad.serveFile(deets.pathname.slice(1),200,{"Content-type":"image/"+extension},response) 
   }
+  else if (deets.pathname == "/sitemap.xml") { rad.serveFile("sitemap.xml",200,{},response) }
+  else { rad.servePage("missing",404,{"Content-type":"text/html"},response) }
 
-}).listen(80);
+}).listen(8000);
--- radicalModule.js
+++ radicalModule.js
@@ -1,44 +1,56 @@
-var fs = require("fs"); var util = require("util");
+var fs = require("fs"); var util = require("util"); var formidable = require("formidable");
 //var mysql = require("mysql"); 
 
-exports.serveFile = (file,status,headers,response) => {
-  fs.readFile(file,(error,fileData) => {
-    response.writeHead(status,headers); 
-    response.end(fileData);
-  });
-};
-
-exports.servePage = (page,status,headers,response) => {
+exports.servePage = (page,status,headers,response,direct=false) => {
   headers["Content-type"] = "text/html";
-  fs.readFile("html/top.html",(errorTop,htmlTop) => {
-    fs.readFile("html/bottom.html",(errorBottom,htmlBottom) => {
-      fs.readFile("html/"+page+".html",(error,fileData) => {
-        response.writeHead(status,headers);         
-        response.end(htmlTop+fileData+htmlBottom);
-      });
+  fs.readFile("html/top.html",(errorTop,htmlTop) => { if (errorTop) { console.error(errorTop); }
+    fs.readFile("html/bottom.html",(errorBottom,htmlBottom) => { if (errorBottom) { console.error(errorBottom) }
+      if (direct) { fileData = page } else { fileData = fs.readFileSync("html/"+page+".html") };
+      response.writeHead(status,headers);         
+      response.end(htmlTop+fileData+htmlBottom);
     });
   });
 }
 
+exports.serveFile = (file,status,headers,response,direct=false) => {
+  if (direct) { fileData = file } 
+  else { 
+    if (fs.existsSync(file)) { fileData = fs.readFileSync(file) } 
+    else { console.error("File \""+file+"\" doesn't exist"); this.servePage("missing",404,{},response); return }
+  }
+  response.writeHead(status,headers); 
+  response.end(fileData);
+};
+
 exports.processPOST = (request,daFunction) => {
-  var formData = {}; var stream;
-  request.on("data", (chunk) => { stream += chunk } )
-  request.on("end", () => { 
-    chunks = [];
-    while (stream.includes("Content-Disposition")) {
-      start = stream.indexOf("Content-Disposition"); stream = stream.slice(start+"Content-Disposition: ".length,-1)+stream.slice(-1,-1); 
-      end = stream.indexOf("Content-Disposition"); 
-      dataString = stream.slice(0,end); output = {};
-      stream = stream.slice(end,-1)+stream.slice(-1,-1);
-      type = dataString.slice(0,dataString.indexOf(";"));
-      if (!type.includes("image")) {
-        dataString = dataString.slice(dataString.indexOf("\"")+1,-1)+dataString.slice(-1,-1);
-        daKey = dataString.slice(0,dataString.indexOf("\"")); dataString = dataString.replace(daKey+"\"","");
-        dataString = dataString = dataString.slice(dataString.indexOf("\"")+1,-1)+dataString.slice(-1,-1);
-        daValue = dataString.slice(0,dataString.indexOf("\"")); dataString = dataString.replace(daValue+"\"","");
-        formData[daKey] = daValue;
-      }
+  var filenameList = [];
+  var form = new formidable.IncomingForm({
+    multiples: true, uploadDir: "temp/", maxFileSize: 500 * 1024 * 1024, keepExtensions: true, 
+    filename: (name, ext, part, form) => { 
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; newName = "";
+      for (let i=0;i<16;i++) { newName += characters.charAt(Math.floor(Math.random()*characters.length)) }
+      filenameList.push(newName+ext);
+      return newName+ext
     }
-    daFunction(formData);
+  }); 
+
+  form.parse(request, function (parseError, fields, files) {
+    if (parseError) { console.log(parseError); daFunction(formData,parseError) } 
+    else {
+    formData = {};
+    for (var key of Object.keys(fields)) { formData[key] = fields[key][0] }
+    formData.filenameList = filenameList;
+    daFunction(formData,false);
+    }
   });
 }
+
+exports.fetchadids = (source, type, options, daFunction=false) => {
+  async function fetchadids(source, type, options, daFunction=false) {
+    if (type == "json") { function process(response){return response.json()} } 
+    else if (type == "text") { function process(response){return response.text()} }
+    else { function process(response){return response.text()} }
+    await fetch(source,options).then(process)
+    .then(data => { if (daFunction != false) { daFunction(data) } else { console.log("fetch at",source,":",data) } })
+  } fetchadids(source,type,options,daFunction);
+};
